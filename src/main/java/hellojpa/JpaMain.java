6026189285615dcf20b3e6804ff10eb64fa548df
joyeonw <jbg0220@naver.com>
2024-05-31 17:39:36 +0900

    5.31 자바ORM표준 JPA 프로그래밍-기본편

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,65 @@
+package hellojpa;
+
+import java.util.List;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+        
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+        	// 비영속
+//        	Member member = new Member();
+//        	member.setId(102L);
+//        	member.setName("HelloC102");
+//        	
+//        	em.persist(member);
+//        	Member findMember = em.find(Member.class, 102L);
+//        	System.out.println("findMember.id = " + findMember.getId());
+//        	System.out.println("findMember.name = " + findMember.getName());
+//        	findMember.setName("HelloJPA");
+        	
+//        	List<Member> result = em.createQuery("select m from Member as m", Member.class)
+//        			.setFirstResult(5)
+//        			.setMaxResults(8)
+//        			.getResultList();
+//        	
+//        	for (Member member : result) {
+//        		System.out.println("member.name = " + member.getName() );
+//        	}
+        	
+//        	System.out.println("findMember.id = " + findMember.getId());
+//        	System.out.println("findMember.name = " + findMember.getName());
+        
+        	// 영속
+//        	Member member = em.find(Member.class, 150L);
+//        	member.setName("zzzzzzz");
+//        	
+//        	System.out.println("==============================");
+        	
+        	Member member = em.find(Member.class, 150L);
+        	member.setName("ddddd");
+        	
+        	em.detach(member);
+        	
+            tx.commit();
+		} catch (Exception e) {
+			e.printStackTrace(); // 
+			tx.rollback();
+		} finally {
+			em.close();
+		}
+        
+        emf.close();
+    }
+}
