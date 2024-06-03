a041a2c589b80bc6f7b746d89dee68bb5de6a8c7
joyeonw <jbg0220@naver.com>
2024-06-03 10:27:17 +0900

    6.03 ex1-hello-jpa 끝

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -11,24 +11,24 @@ public class JpaMain {
 
     public static void main(String[] args) {
     	EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
-
         EntityManager em = emf.createEntityManager();
         
         EntityTransaction tx = em.getTransaction();
         tx.begin();
 
-        try {
-        	// 비영속
+//        try {
+//        	// 비영속
 //        	Member member = new Member();
-//        	member.setId(102L);
-//        	member.setName("HelloC102");
-//        	
+//        	member.setId(104L);
+//        	member.setName("HelloC104");
+////        	
 //        	em.persist(member);
+//        	 tx.commit();
 //        	Member findMember = em.find(Member.class, 102L);
 //        	System.out.println("findMember.id = " + findMember.getId());
 //        	System.out.println("findMember.name = " + findMember.getName());
 //        	findMember.setName("HelloJPA");
-        	
+//        	
 //        	List<Member> result = em.createQuery("select m from Member as m", Member.class)
 //        			.setFirstResult(5)
 //        			.setMaxResults(8)
@@ -37,29 +37,49 @@ public class JpaMain {
 //        	for (Member member : result) {
 //        		System.out.println("member.name = " + member.getName() );
 //        	}
-        	
+//        	
 //        	System.out.println("findMember.id = " + findMember.getId());
 //        	System.out.println("findMember.name = " + findMember.getName());
-        
-        	// 영속
+//        
+//        	// 영속
 //        	Member member = em.find(Member.class, 150L);
 //        	member.setName("zzzzzzz");
+////        	
+//       	System.out.println("==============================");
 //        	
-//        	System.out.println("==============================");
+//        	Member member = em.find(Member.class, 150L);
+//        	member.setName("ddddd");
+//        	
+//        	em.detach(member);
         	
-        	Member member = em.find(Member.class, 150L);
-        	member.setName("ddddd");
+//            tx.commit();
+        try {
+        	Member member1 = new Member();
+        	member1.setUsername("A");
+
+        	Member member2 = new Member();
+        	member2.setUsername("B");
+
+        	Member member3 = new Member();
+        	member3.setUsername("C");        	
         	
-        	em.detach(member);
+        	System.out.println("===========================");
+        	em.persist(member1); // 1, ,51
+        	em.persist(member2);// 
+        	em.persist(member3);
         	
-            tx.commit();
+        	System.err.println("member1 = " + member1.getId());
+        	System.err.println("member2 = " + member2.getId());
+        	System.err.println("member3 = " + member3.getId());
+        	
+        	System.out.println("===========================");
+        	tx.commit();
 		} catch (Exception e) {
-			e.printStackTrace(); // 
 			tx.rollback();
 		} finally {
 			em.close();
 		}
-        
+        	
         emf.close();
     }
 }
